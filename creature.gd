3e3cdb367febd1271b9b5700dd6e94dd4cb599c8
MBorchert <matthew.borchert@lhssc.org>
2024-11-20 17:34:05 -0600

    added creature

---
+++ creature.gd
@@ -0,0 +1,47 @@
+extends Node2D
+
+var velocity = Vector2()
+var max_speed = 5
+
+var change_point = 10
+var current_pos: Vector2
+var max_dst = 0
+var change_speed = 1
+
+var toward_point_weight = 40
+
+var timi: Timer
+
+@export var camera: Camera2D
+
+func toward_point():
+	return (current_pos - position).normalized()
+
+func limit_speed(vel: Vector2):
+	if (vel.length() > max_speed):
+		return vel.normalized() * max_speed
+	else:
+		return vel
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	max_dst = get_viewport_rect().size / 2 + Vector2(50, 50)
+	random_screen_point()
+
+	timi = $"Timer"
+	timi.stop()
+	timi.start(change_speed)
+	timi.connect("timeout", random_screen_point)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	velocity += toward_point() * toward_point_weight * delta
+	velocity = limit_speed(velocity)
+	position += velocity
+
+	if ((position - current_pos).length() < change_point):
+		random_screen_point()
+		timi.start(change_speed)
+
+func random_screen_point():
+	current_pos = camera.position + (Vector2(randf() * 2 - 1, randf() * 2 - 1) * max_dst)
